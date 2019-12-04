e2781174d607a49351b950b9a5df460b0f77cef2
donovanwest <donovanwest@F1092-10-2098>
2019-12-04 14:26:28 -0600

    :)

--- ShapeSorter/Program.cs
+++ ShapeSorter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ShapeSorter
 {
@@ -7,6 +9,77 @@ namespace ShapeSorter
         static void Main(string[] args)
         {
             Console.WriteLine("Fun With Shapes!");
+            List<IShape> shapes = new List<IShape>()
+            {
+                new Circle(4.0),
+                new Rectangle(6, 7),
+                new Square(5.0),
+                new Circle(3.0),
+                new Rectangle(2,4),
+                new Circle(5),
+                new Square(10),
+            };
+            Console.WriteLine();
+            foreach(IShape shape in shapes)
+            {
+                Console.WriteLine($"Area of shape is {shape.Area}");
+            }
+            Console.WriteLine("-------------------------");
+
+            IEnumerable<IShape> filteredShapes = shapes.Where(shape => shape.Area > 50);
+            Console.WriteLine("Shapes with an area > 50");
+            foreach (IShape shape in filteredShapes)
+            {
+                Console.WriteLine($"Area of shape is {shape.Area}");
+            }
+            Console.WriteLine("-------------------------");
+
+            IEnumerable<Circle> circles = shapes.OfType<Circle>();
+
+            Console.WriteLine("Circles");
+            foreach (Circle circle in circles)
+            {
+                Console.WriteLine($"Circle with radius {circle.Radius} and area {circle.Area}");
+            }
+            Console.WriteLine("-------------------------");
+            IEnumerable<Circle> filteredCircles = shapes.OfType<Circle>().Where(c => c.Area < 30);
+            Console.WriteLine("Circles with area < 30");
+            foreach (Circle circle in filteredCircles)
+            {
+                Console.WriteLine($"Circle with radius {circle.Radius} and area {circle.Area}");
+            }
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("Grouping By Area");
+            var groupByArea = shapes.GroupBy(shape => shape.Area % 2 == 0);
+            foreach(var group in groupByArea)
+            {
+                Console.WriteLine(group.Key ? "Evens" : "Odds");
+                foreach(var shape in group)
+                {
+                    Console.WriteLine($"Area of shape is {shape.Area}");
+                }
+            }
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("Grouping By Type");
+
+            var groupByType = shapes.OrderBy(shape => shape.Area).GroupBy(shape => shape.GetType());
+            foreach(var group in groupByType)
+            {
+                //Console.WriteLine($"Shapes of type {group.Key.Name}");
+                foreach(var shape in group)
+                {
+                    Console.WriteLine($"{group.Key.Name} with area {shape.Area}");
+                }
+            }
+
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
